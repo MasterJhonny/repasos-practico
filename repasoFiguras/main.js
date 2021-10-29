78b79e39704184c1415b7845839cc997bef29763
MasterJhonny <jhonnyeddy.22@gmail.com>
2021-10-29 03:35:22 -0400

    fisrt commit

---
+++ repasoFiguras/main.js
@@ -0,0 +1,47 @@
+// code from cuadrado
+const ladoCadrado = 5
+console.group("Cuadrado")
+console.log(ladoCadrado)
+
+const perimeterCuadrado = ladoCadrado * 4
+console.log(perimeterCuadrado)
+
+const areaCuadrado = ladoCadrado**2
+console.log(areaCuadrado)
+console.groupEnd()
+// code from triangular
+const ladoTriangular1 = 6
+const ladoTriangular2 = 6
+const baseTriangular = 4
+const alturaTriangular = 5.2
+console.group("Triangulo")
+
+console.log(ladoTriangular1, ladoTriangular2, baseTriangular)
+
+const perimeterTrinagular = ladoTriangular1 + ladoTriangular2 + baseTriangular
+console.log(perimeterTrinagular)
+
+const areaTRinagular = (baseTriangular * alturaTriangular)/2
+console.log(areaTRinagular)
+console.groupEnd()
+
+// code circunference
+const radioCircunference = 4
+const PI = Math.PI
+console.group("Cuadrado")
+
+console.log(radioCircunference)
+console.log(PI)
+const diametroCircunference = radioCircunference * 2
+console.log(diametroCircunference)
+
+const perimeterCircunference = diametroCircunference * PI
+console.log(perimeterCircunference)
+
+const areaCircunference = radioCircunference**2 * PI
+console.log(areaCircunference)
+console.groupEnd()
+
+
+
+ 
